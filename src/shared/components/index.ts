f1bc62e2b26deddfe3f2ae80254cf2e64113397c
4Forsythe <forsythe.dev@gmail.com>
2025-04-05 20:35:30 +0400

    Add shared/components reexports

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,4 +1,8 @@
 export * from './chat';
 export * from './sidebar';
 
+export { ErrorBlock } from './error-block';
+export { NotFound } from './not-found';
+export { ModelItem } from './model-item';
 export { LogoutButton } from './logout-button';
+export { WelcomeAnimation } from './welcome-animation';
