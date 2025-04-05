097d43c4722843e06951d8bc98bc70192c4884e4
4Forsythe <forsythe.dev@gmail.com>
2025-04-05 20:32:49 +0400

    Update forms

--- src/shared/forms/auth-form.tsx
+++ src/shared/forms/auth-form.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import { X } from 'lucide-react';
 
+import { WelcomeAnimation } from '@/shared/components';
 import { Button, FormField, Heading } from '@/shared/ui';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { selectAuth, login, setAuthLoading } from '@/entities/auth';
 
 const FormWrapper = styled.div`
   width: 459px;
@@ -14,7 +18,7 @@ const FormWrapper = styled.div`
   background-color: rgb(var(--secondary-color));
 `;
 
-const FormContainer = styled.div`
+const FormContainer = styled.form`
   flex: 1;
   gap: 16px;
   display: flex;
@@ -44,21 +48,64 @@ const FormHeaderCloseButton = styled.button`
 `;
 
 export const AuthForm: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { isLoading } = useAppSelector(selectAuth);
+
+  const [loginValue, setLoginValue] = React.useState('');
+  const [passwordValue, setPasswordValue] = React.useState('');
+
+  /*
+    В ИДЕАЛЕ, конечно, React Hook Form + zod/yup, и у меня на GitHub есть достаточно много проектов с их использованием,
+    Тут я делать НЕ СТАЛ, поскольку был увлечен реализацией других фич. Надеюсь на ваше понимание 😘 (это большая экономия времени для меня).
+  */
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (loginValue && passwordValue) {
+      dispatch(setAuthLoading(true));
+
+      setTimeout(() => {
+        dispatch(login({ login: loginValue, password: passwordValue }));
+        dispatch(setAuthLoading(false));
+        navigate('/', { replace: true });
+      }, 2500);
+    }
+  };
+
   return (
-    <FormWrapper>
-      <FormContainer>
-        <FormHeader>
-          <Heading text='Авторизация' />
-          <FormHeaderCloseButton>
-            <X size={24} />
-          </FormHeaderCloseButton>
-        </FormHeader>
-        <FormField label='Логин' placeholder='Ваш логин' required />
-        <FormField label='Пароль' placeholder='Ваш пароль' required />
-        <Button style={{ width: '100%' }} type='button'>
-          Войти
-        </Button>
-      </FormContainer>
-    </FormWrapper>
+    <React.Fragment>
+      <WelcomeAnimation user={loginValue} isVisible={isLoading} />
+      <FormWrapper>
+        <FormContainer onSubmit={handleSubmit}>
+          <FormHeader>
+            <Heading as='h5' text='Авторизация' />
+            <FormHeaderCloseButton aria-label='Закрыть окно' type='button'>
+              <X size={24} />
+            </FormHeaderCloseButton>
+          </FormHeader>
+          <FormField
+            label='Логин'
+            placeholder='Ваш логин'
+            value={loginValue}
+            onChange={(e) => setLoginValue(e.target.value)}
+            autoComplete='off'
+            required
+          />
+          <FormField
+            label='Пароль'
+            type='password'
+            placeholder='Ваш пароль'
+            value={passwordValue}
+            onChange={(e) => setPasswordValue(e.target.value)}
+            required
+          />
+          <Button type='submit' style={{ width: '100%' }} isLoading={isLoading}>
+            Войти
+          </Button>
+        </FormContainer>
+      </FormWrapper>
+    </React.Fragment>
   );
 };
--- src/shared/forms/chat-form.tsx
+++ src/shared/forms/chat-form.tsx
@@ -1,30 +1,108 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 import { Selector } from '@/shared/ui';
-import { ChatInput } from '@/shared/components';
-import { mockChatFormModels } from '@/shared/mocks';
+import { ChatInput, ModelItem } from '@/shared/components';
+import { useGetModelsQuery } from '@/entities/model';
+import { useCreateChatMutation } from '@/entities/chat';
+import { useCreateMessageMutation } from '@/entities/message';
 
-const ChatFormWrapper = styled.div`
+const ChatFormWrapper = styled.form`
   gap: 14px;
   display: flex;
   flex-direction: column;
 `;
 
-export const ChatForm: React.FC = () => {
+interface Props {
+  chatId?: string;
+}
+
+export const ChatForm: React.FC<Props> = ({ chatId }) => {
+  const navigate = useNavigate();
+
+  const [inputValue, setInputValue] = React.useState('');
   const [targetModel, setTargetModel] = React.useState(0);
 
+  const { data: models, isLoading, isError, isSuccess } = useGetModelsQuery();
+
+  const modelItems = React.useMemo(() => {
+    return (
+      models?.map((model) => <ModelItem key={model.id} {...model} />) || []
+    );
+  }, [models]);
+
+  const disabledItems = React.useMemo(() => {
+    return (
+      models
+        ?.map((model, index) =>
+          model.allowed_plan_type !== null && model.allowed_plan_type !== 'FREE'
+            ? index
+            : -1
+        )
+        .filter((index) => index !== -1) || []
+    );
+  }, [models]);
+
+  const [createChat, { isLoading: isPendingChat }] = useCreateChatMutation();
+  const [createMessage, { isLoading: isPendingMessage }] =
+    useCreateMessageMutation();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!inputValue.trim()) return;
+
+    try {
+      if (chatId) {
+        return createMessage({
+          chatId: chatId,
+          message: inputValue,
+        });
+      }
+
+      const chat = await createChat({
+        name: 'Новый чат',
+        modelId: models ? models[targetModel].id : undefined,
+      }).unwrap();
+
+      await createMessage({
+        chatId: chat.id,
+        message: inputValue,
+      }).unwrap();
+
+      navigate(`/chat/${chat.id}`);
+    } catch (error) {
+      console.error('[chat-form]', error);
+      toast.error('Произошла ошибка во время создания чата');
+    } finally {
+      setInputValue('');
+    }
+  };
+
   return (
-    <ChatFormWrapper>
-      <Selector
-        direction='up'
-        values={mockChatFormModels}
-        targetIndex={targetModel}
-        onChange={(index) => setTargetModel(index)}
-      >
-        {mockChatFormModels[targetModel]}
-      </Selector>
-      <ChatInput />
+    <ChatFormWrapper onSubmit={handleSubmit}>
+      {!chatId && (
+        <Selector
+          direction='up'
+          values={modelItems}
+          targetIndex={targetModel}
+          disabled={isError}
+          disabledItems={disabledItems}
+          isLoading={isLoading}
+          onChange={(index) => setTargetModel(index)}
+        >
+          {isSuccess && modelItems
+            ? modelItems[targetModel]
+            : 'Возникла ошибка'}
+        </Selector>
+      )}
+      <ChatInput
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        isLoading={isPendingChat || isPendingMessage}
+      />
     </ChatFormWrapper>
   );
 };
